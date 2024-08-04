cee48aec029893b07b35f7d3e5c2266293896f15
Czimerin <nick.kogovsek@gmail.com>
2024-08-04 10:30:20 -0400

    Setup initial project structure

---
+++ Clarity-Ventures-Internal-Assignment/EmailSenderLibary/IEmailService.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Clarity_Ventures_Internal_Assignment.EmailSenderLibary.Models;
+
+namespace Clarity_Ventures_Internal_Assignment.EmailSenderLibary
+{
+    public interface IEmailService
+    {
+        Task SendEmailAsync(EmailModel email);
+    }
+}
