66fc2480ffcb42d89ab4c115b384c0a963a7f522
Czimerin <nick.kogovsek@gmail.com>
2024-08-07 11:04:52 -0400

    Add base Email Service Test structure

---
+++ Tests/Services.Tests/EmailServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using Moq;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
+using MimeKit;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Services.EmailService;
+using Core.Models;
+
+namespace Tests
+{
+    public class EmailServiceTests
+    {
+
+    }
+}
